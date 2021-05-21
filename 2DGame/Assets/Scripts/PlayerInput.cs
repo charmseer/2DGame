6e837801d4ed66d69b62beccd41293fc5c994c59
charmseer <charmseer@gmail.com>
2021-05-21 22:48:56 +0530

    Setup Player movement using Unity Input System

--- 2DGame/Assets/Scripts/PlayerInput.cs
+++ 2DGame/Assets/Scripts/PlayerInput.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public InputAction movement;
+    public CharacterController controller;
+
+    private void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+    }
+    private void OnEnable()
+    {
+        movement.Enable();
+    }
+    private void OnDisable()
+    {
+        movement.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 inputVector = movement.ReadValue<Vector2>();
+        Vector3 finalVector = new Vector3();
+
+        finalVector.x = inputVector.x;
+        finalVector.z = inputVector.y;
+        // Debug.Log(movement.ReadValue<Vector2>().ToString());
         
+        controller.Move(finalVector * Time.deltaTime * 10f);
     }
 }
