3f4d9aa5404b9b0dab6b505b7c5df06d190c827e
charmseer <charmseer@gmail.com>
2021-05-23 01:00:34 +0530

    v0 Make Player deploy projectile on Spacebar press
    Relevant line of code:
    
     Keyboard.current.spaceKey.wasPressedThisFrame
    
    Issue : If player fires while moving forward, player moves with projectile. :/

--- 2DGame/Assets/Scripts/PlayerController.cs
+++ 2DGame/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // Script uses the new Unity Input controller 23rd May 2021
+
     public InputAction movement;
     public float speed = 10.0f;
     public CharacterController controller;
+
+    //
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
 
     void OnEnable()
@@ -30,6 +36,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
         //Debug.Log(movement.ReadValue<Vector2>().ToString());
         Vector2 inputVector = movement.ReadValue<Vector2>();
 
@@ -37,8 +47,7 @@ public class PlayerController : MonoBehaviour
         finalVector.x = inputVector.x;
         finalVector.z = inputVector.y;
 
-
-
         controller.Move(finalVector * Time.deltaTime * 10f);
+
     }
 }
