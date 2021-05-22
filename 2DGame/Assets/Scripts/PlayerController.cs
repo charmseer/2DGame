df22fa06a06c79d00338cefd9d990ac0aed3e16d
charmseer <charmseer@gmail.com>
2021-05-22 22:37:09 +0530

    Added back the New Input System to v0
    Will figure out how to make it work later.

--- 2DGame/Assets/Scripts/PlayerController.cs
+++ 2DGame/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalInput;
+    public InputAction movement;
     public float speed = 10.0f;
+    public CharacterController controller;
     // Start is called before the first frame update
+
+    void OnEnable()
+    {
+        movement.Enable();
+        
+    }
+
+    void OnDisable()
+    {
+        movement.Disable();
+    }
+
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        //Debug.Log(movement.ReadValue<Vector2>().ToString());
+        Vector2 inputVector = movement.ReadValue<Vector2>();
+
+        Vector3 finalVector = new Vector3();
+        finalVector.x = inputVector.x;
+        finalVector.z = inputVector.y;
+
+
+
+        controller.Move(finalVector * Time.deltaTime * 10f);
     }
 }
