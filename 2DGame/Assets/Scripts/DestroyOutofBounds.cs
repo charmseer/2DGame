f486ab59048ccb08b6bfb834a317e975237bd726
charmseer <charmseer@gmail.com>
2021-05-23 15:25:23 +0530

    v0 Create Enemy prefabs, Setup motion restriction scripts

---
+++ 2DGame/Assets/Scripts/DestroyOutofBounds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutofBounds : MonoBehaviour
+{
+    private float topBound = 26.0f;
+    private float lowerBound = 26.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x > topBound)
+        {
+            Destroy(gameObject);
+        }
+
+        else if (transform.position.x < -lowerBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- 2DGamev1/Assets/Scripts/MoveForwards.cs
+++ 2DGamev1/Assets/Scripts/MoveForwards.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveForwards : MonoBehaviour
 {
-    private float speed = 40.0f;
+    public float speed = 40.0f;
     // Start is called before the first frame update
     void Start()
     {
