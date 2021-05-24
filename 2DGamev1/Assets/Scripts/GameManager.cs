22201f0beab6115f0bd3ea6efc097dadbf39211e
charmseer <charmseer@gmail.com>
2021-05-24 12:57:26 +0530

    Create GameOver Panel and Restart cycle

--- 2DGamev1/Assets/Scripts/GameManager.cs
+++ 2DGamev1/Assets/Scripts/GameManager.cs
@@ -1,23 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
-
+    bool gameHasEnded = false;
+    public GameObject GameOverUI;
     public void EndGame()
     {
+        
         Debug.Log("Game Over");
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            GameOverUI.SetActive(true);
+            Invoke("Restart", 3);
+        }
     }
-    // Start is called before the first frame update
-    void Start()
+    void Restart()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
